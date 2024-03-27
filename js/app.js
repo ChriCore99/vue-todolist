d47420464726389c1bc79167da4096323323d042
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-27 15:33:05 +0100

    ADD: mindstone 1 e 2 completate

--- js/app.js
+++ js/app.js
@@ -3,7 +3,29 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        todos:[
+            {
+                text: 'fare la spesa',
+                done: false,
+            },
+
+            {
+                text: 'andare in palestra',
+                done: true,
+            },
+
+            {
+                text: 'cenare con trump',
+                done: false,
+            }
+        ]
       }
+    },
+
+    methods: {
+       removeTodo(todo, i){
+        this.todos.splice(i, 1)
+       } 
     }
+
   }).mount('#app')

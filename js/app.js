44ff5fa866172e3db61711952505088ba1a2a515
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-27 16:32:10 +0100

    ADD: mindstone 3 completata

--- js/app.js
+++ js/app.js
@@ -15,17 +15,57 @@ const { createApp } = Vue
             },
 
             {
+                text: 'petinare il gato',
+                done: true,
+            }, 
+
+            {
+                text: 'passeggiata con amante',
+                done: false,
+            },
+
+            {
+                text: 'aperitivo con fidanzata',
+                done: true,
+            },
+
+            {
                 text: 'cenare con trump',
                 done: false,
             }
-        ]
+        ],
+
+        newTodo: '',
+
       }
     },
 
     methods: {
        removeTodo(todo, i){
         this.todos.splice(i, 1)
-       } 
+       },
+
+       fatto(todo){
+        if(todo.done === false){
+            todo.done = true
+        } else {
+            todo.done = false
+        }
+       },
+
+       addTodo(){
+        if(this.newTodo !== ''){
+            this.newTodo = {
+                text: this.newTodo,
+                done: false
+            };
+            this.todos.push(this.newTodo);
+            this.newTodo='';
+        } else {
+            alert("scrivi una task")
+        }
+       }
+
     }
 
   }).mount('#app')
